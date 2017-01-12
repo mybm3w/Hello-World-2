41a21b13626909ebb1220ef6daa870b2fab287c5
Anton Holtzhausen <afh13@outlook.com>
2017-01-12 22:14:08 +1100

    Back to the orginal file

--- SetDHCPAddress.ps1
+++ SetDHCPAddress.ps1
@@ -1,5 +1,5 @@
 ﻿$netadapter = "vEthernet (External)"
-write-host -ForegroundColor Green "Setting the Ethernet adaptor to DHCP. Changed the text. Updated from Notepad"
+write-host -ForegroundColor Green "Setting the Ethernet adaptor to DHCP."
 Set-NetIPInterface $netadapter -Dhcp Enabled
 write-host -ForegroundColor Green "Paused - Wait few seconds before continue."
 pause
