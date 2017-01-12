d07e59b6d1965b473ac9e4412ef59c8926e3d27d
Anton Holtzhausen <afh13@outlook.com>
2017-01-12 22:23:11 +1100

    New branch commit

--- SetDHCPAddress.ps1
+++ SetDHCPAddress.ps1
@@ -1,5 +1,5 @@
 ﻿$netadapter = "vEthernet (External)"
-write-host -ForegroundColor Green "Setting the Ethernet adaptor to DHCP."
+write-host -ForegroundColor Green "Setting the Ethernet adaptor to DHCP. New branch update"
 Set-NetIPInterface $netadapter -Dhcp Enabled
 write-host -ForegroundColor Green "Paused - Wait few seconds before continue."
 pause
