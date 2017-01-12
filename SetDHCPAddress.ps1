53248a0a776314f379fc7d9ec44a224b9d23c2ba
Anton Holtzhausen <afh13@outlook.com>
2017-01-12 21:57:56 +1100

    Made a another change to a comment

--- SetDHCPAddress.ps1
+++ SetDHCPAddress.ps1
@@ -1,7 +1,7 @@
 ﻿$netadapter = "vEthernet (External)"
 write-host -ForegroundColor Green "Setting the Ethernet adaptor to DHCP. Changed the text. Updated from Notepad"
 Set-NetIPInterface $netadapter -Dhcp Enabled
-write-host -ForegroundColor Green "Paused - Wait few seconds before continue. I made a change"
+write-host -ForegroundColor Green "Paused - Wait few seconds before continue. I made a change. Change this line again"
 pause
 write-host -ForegroundColor Yellow $newnetIP.InterfaceAlias $newnetIP.IPAddress"/"$newnetIP.PrefixLength
 
