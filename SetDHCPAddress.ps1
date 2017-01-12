f69374b128f87f0f77604bc3200eb190c466ab47
Anton Holtzhausen <afh13@outlook.com>
2017-01-12 18:27:23 +1100

    My commit message

---
+++ SetDHCPAddress.ps1
@@ -0,0 +1,10 @@
+﻿$netadapter = "vEthernet (External)"
+write-host -ForegroundColor Green "Setting the Ethernet adaptor to DHCP. Changed the text. Updated from Notepad"
+Set-NetIPInterface $netadapter -Dhcp Enabled
+write-host -ForegroundColor Green "Paused - Wait few seconds before continue"
+pause
+write-host -ForegroundColor Yellow $newnetIP.InterfaceAlias $newnetIP.IPAddress"/"$newnetIP.PrefixLength
+
+
+
+
